6455e9cc3758f2d6c54861a7d55182befaba214a
TARUSSI <tarussisingh@gmail.com>
2022-06-16 12:05:01 +0530

    Added comments

--- Eyes_on_screen/js/index.js
+++ Eyes_on_screen/js/index.js
@@ -1,20 +1,30 @@
 import {updateRandomKey, addJiggleClass, checkIfSameKeys, removeJiggleClass} from './functions.js';
 
+// list of all the keys on the keyboard
 const keys = document.querySelectorAll(".key");
+// variables to store the classes and attributes that need to be changed
 const classToChange = "jiggle";
 const attributeToCheck = "data-key";
 
+// selecting a key randomly
 let randomKey = updateRandomKey(keys);
 
+// function starts and stops toggling based on the key pressed
 const repeatToggle = () => {
 
+    // adding jiggle class to the randomly selected key to make it jiggle
     addJiggleClass(randomKey, classToChange);
 
+    // listening to the keydown event
     document.addEventListener("keydown", function (event) {
 
+        // checking if correct key was pressed or not
         if (checkIfSameKeys(event, randomKey, attributeToCheck)) {
+            // removing jiggle class from the randomly selected key to stop the jiggling
             removeJiggleClass(randomKey, classToChange);
+            // choosing another key and updating the value of the randomly selected key
             randomKey = updateRandomKey(keys);
+            // calling the function again to do the entire process on repeat
             repeatToggle();
         }
     });
