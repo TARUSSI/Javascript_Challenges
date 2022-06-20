2167544e2d3eda31fccf56f544d99fb49048f92f
TARUSSI <tarussisingh@gmail.com>
2022-06-20 17:33:12 +0530

    Added comments

--- Piano/js/functions.js
+++ Piano/js/functions.js
@@ -1,20 +1,27 @@
 import {keys, baseUrl, valueToBeChangedInBaseUrl} from "./variables.js"
 
+// function finds the position of the element in the nodeList
 const getIndex = (element, nodeList) => {
     return Array.prototype.indexOf.call(nodeList, element);
 }
 
+// function makes changes in the base url
 const getEditedUrl = (oldValue, newValue, baseUrl) => {
     return baseUrl.replace(oldValue, newValue);
 }
 
+// function plays an audio
 const playSound = (url) => {
     let audio = new Audio(url);
     audio.play();
 }
 
+// function picks an audio for the piano key and plays it
 export function playPiano() {
+    // finding the position of the key pressed in the array of all keys
+    // the +1 is due to audio numbering starting from 1 not 0
     let audioNumber = getIndex(this, keys) + 1;
+    // editing the base url to find the audio to be played for that key
     let url = getEditedUrl(valueToBeChangedInBaseUrl, audioNumber, baseUrl);
     playSound(url);
 }
--- Piano/js/variables.js
+++ Piano/js/variables.js
@@ -1,3 +1,6 @@
+// variable to store all the piano keys
 export const keys = document.querySelectorAll("a");
+// variable to store the base url of the audio files
 export const baseUrl = "./audio/key-#.mp3";
+// variable to store the symbol that is to be changed in the base url
 export const valueToBeChangedInBaseUrl = "#";
