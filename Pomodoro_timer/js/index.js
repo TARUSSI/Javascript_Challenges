e962478a81f500d03fa24a199301868c1ce18dc9
TARUSSI <tarussisingh@gmail.com>
2022-06-20 13:43:38 +0530

    Added challenge 1 files

---
+++ Pomodoro_timer/js/index.js
@@ -0,0 +1,5 @@
+import {startStopButton, settingButton} from "./variables.js";
+import {clickStart, clickSetting} from "./functions.js";
+
+startStopButton.addEventListener("click", clickStart);
+settingButton.addEventListener("click", clickSetting);
---
+++ Pomodoro_timer/js/variables.js
@@ -0,0 +1,5 @@
+export const ring = document.querySelector(".ring");
+export const startStopButton = document.querySelector(".start");
+export const settingButton = document.querySelector(".settings");
+export const minutes = document.querySelector(".minutes input");
+export const seconds = document.querySelector(".seconds input");
