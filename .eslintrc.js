3602a8f34442af65a248693265249fe85b9de677
TARUSSI <tarussisingh@gmail.com>
2022-06-24 15:43:12 +0530

    github action

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,49 @@
+// module.exports = {
+//   env: {
+//     browser: true,
+//     es2021: true,
+//     node: true,
+//   },
+//   extends: [
+//     "eslint:recommended",
+//     "plugin:react/recommended",
+//     "plugin:prettier/recommended",
+//     "plugin:@typescript-eslint/recommended",
+//   ],
+//   parserOptions: {
+//     ecmaFeatures: {
+//       jsx: true,
+//     },
+//     ecmaVersion: "latest",
+//     sourceType: "module",
+//   },
+//   plugins: ["react", "prettier", "@typescript-eslint"],
+//   rules: {
+//     "prettier/prettier": [
+//       "error",
+//       {
+//         endOfLine: "auto",
+//       },
+//     ],
+//     semi: ["error", "always"],
+//     quotes: ["error", "double"],
+//     "arrow-body-style": "off",
+//     "prefer-arrow-callback": "off",
+//   },
+// };
+
 module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true,
   },
-  extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:prettier/recommended",
-    "plugin:@typescript-eslint/recommended",
-  ],
+  extends: ["standard", "prettier"],
   parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
     ecmaVersion: "latest",
     sourceType: "module",
   },
-  plugins: ["react", "prettier", "@typescript-eslint"],
   rules: {
-    "prettier/prettier": [
-      "error",
-      {
-        endOfLine: "auto",
-      },
-    ],
-    semi: ["error", "always"],
-    quotes: ["error", "double"],
-    "arrow-body-style": "off",
-    "prefer-arrow-callback": "off",
+    "linebreak-style": 0,
+    indent: ["error", 4],
   },
 };
---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.export = {
+  bracketSpacing: false,
+  semi: true,
+  singleQuote: true,
+  trailingComma: "all",
+  printWidth: 80,
+  tabWidth: 4,
+  quoteProps: "preserve",
+  endOfLine: "auto",
+};
